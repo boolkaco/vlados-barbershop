3341697e42248f027b4c7af54eb69a83b374d748
Kostiantyn Chaliy <chalyykostyadante@gmail.com>
2024-06-03 20:56:51 +0300

    add ol-map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="map">
-    <gmp-map center="50.06711959838867,14.39958667755127" zoom="16" map-id="DEMO_MAP_ID">
-      <gmp-advanced-marker position="50.06711959838867,14.39958667755127" title="My location"></gmp-advanced-marker>
-    </gmp-map>
-  </div>
+  <div ref="mapContainer" class="map-container"></div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
 
-  
+const mapContainer = ref<HTMLDivElement | null>(null);
+
+const markerCoordinates = [14.39958667755127, 50.06711959838867];
+
+onMounted(() => {
+  if (!mapContainer.value) return;
+
+  // Создание карты
+  const map = new Map({
+    target: mapContainer.value,
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: fromLonLat(markerCoordinates),
+      zoom: 17
+    })
+  });
+
+  // Создание маркера
+  const marker = new Feature({
+    geometry: new Point(fromLonLat(markerCoordinates))
+  });
+
+  marker.setStyle(new Style({
+    image: new Icon({
+      src: 'https://openlayers.org/en/v6.5.0/examples/data/icon.png',
+      anchor: [0.5, 1],
+      scale: 0.7
+    })
+  }));
+
+  const vectorSource = new VectorSource({
+    features: [marker]
+  });
+
+  const markerLayer = new VectorLayer({
+    source: vectorSource
+  });
+
+  map.addLayer(markerLayer);
+});
 </script>
 
-<style scoped lang="scss">
-
-
-
+<style scoped>
+.map-container {
+  height: 504px;
+  width: 100%;
+}
 </style>
